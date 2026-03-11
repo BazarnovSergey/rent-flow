fcad67b9c84232c7e7608bc619a129d035e80589
s.bazarnov <s.bazarnov@lancelot-it.ru>
2026-03-11 23:44:45 +0300

    Initial commit

---
+++ common/src/main/java/ru/rentflow/common/CommonConstants.java
@@ -0,0 +1,13 @@
+package ru.rentflow.common;
+
+/**
+ * Common constants for RentFlow microservices
+ */
+public class CommonConstants {
+    
+    private CommonConstants() {
+        // Utility class
+    }
+    
+    public static final String API_VERSION = "/api/v1";
+}
